28a3c36d585c807af60cb081a318ee12a93a384b
Drashtii <111189432+DrashtiRanpariya@users.noreply.github.com>
2023-08-14 08:57:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+let circleX = 33 ;
+let incX=5;
+let circleY = 296 ;
+let incY =5;
+
+function draw() {
+  // background(0);
+  noStroke();
+  fill(255);
+  circle(circleX,circleY,10);
+  if(circleX>=width)
+  {
+    incX=abs(incX)*-1 
+    //abs(x),gives absolute value of x, |x|,always +ve
+  }
+  else if(circleX<=0)
+  {
+    incX=abs(incX)
+  }
+  if(circleY>=height)
+  {
+    incY=abs(incY)*-1
+  }
+  else if(circleY<=0)
+  {
+    incY=abs(incY)
+  }
+  circleX = circleX + incX*random() ;
+  circleY = circleY + incY*random() ;
+}
+
+
